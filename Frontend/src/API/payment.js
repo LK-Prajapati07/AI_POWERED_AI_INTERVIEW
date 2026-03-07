2143512911312a0b3f20bd791f87d47268aa74ee
Lalit Kumar <prajapatilalitkumar263@outlook.com>
2026-03-07 19:19:24 +0530

    Update payment.js

--- Frontend/src/API/payment.js
+++ Frontend/src/API/payment.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://ai-powered-ai-interview.onrender.com,
+  baseURL: "https://ai-powered-ai-interview.onrender.com",
   withCredentials: true,
 });
 
