be629a9b61ed8ca8888b2e9a34ee9523a32a71ea
Lalit Kumar <prajapatilalitkumar263@outlook.com>
2026-03-07 19:00:35 +0530

    Update interview.js

--- Frontend/src/API/interview.js
+++ Frontend/src/API/interview.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_baseURL, 
+  baseURL: https://ai-powered-ai-interview.onrender.com, 
   withCredentials: true,
 });
 
