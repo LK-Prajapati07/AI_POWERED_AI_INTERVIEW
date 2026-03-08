372f3326d93af26ead08160cba321e67d6c97608
Lalit Kumar <prajapatilalitkumar263@outlook.com>
2026-03-08 07:12:45 +0530

    Update axios.js

--- Frontend/src/API/axios.js
+++ Frontend/src/API/axios.js
@@ -1,21 +1,26 @@
 import axios from "axios";
+
 const API = axios.create({
   baseURL: "https://ai-powered-ai-interview-1.onrender.com",
   withCredentials: true,
 });
+
 export const loginAPI = async (payload) => {
-  const res = await API.post("/auth/firebase-login", payload);
+  const res = await API.post("/api/auth/firebase-login", payload);
   return res.data;
 };
-export const logoutApi=async ()=>{
-  const res=await API.post('/auth/logout')
-  return res.data
-}
-export const getCurrentUser=async()=>{
-  const res=await API.get('/auth/me')
-  return res.data
-}
-export const protectUserAPI=async()=>{
-  const res=await API.get('/auth/protected')
-  return res.data
-}
+
+export const logoutApi = async () => {
+  const res = await API.post("/api/auth/logout");
+  return res.data;
+};
+
+export const getCurrentUser = async () => {
+  const res = await API.get("/api/auth/me");
+  return res.data;
+};
+
+export const protectUserAPI = async () => {
+  const res = await API.get("/api/auth/protected");
+  return res.data;
+};
